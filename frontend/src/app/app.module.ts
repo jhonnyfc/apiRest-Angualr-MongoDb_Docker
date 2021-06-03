5fe57d36be4e9830055f8877c1ef57af5d043b64
jhonnyfc <jhonnyfc88@gmail.com>
2021-06-03 21:41:48 +0200

    alta usuario creada

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,9 @@ import { AltausuariosComponent } from './components/altausuarios/altausuarios.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/altausuarios/altausuarios.component.ts
+++ frontend/src/app/components/altausuarios/altausuarios.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from 'src/app/utils/interfaces';
 
 @Component({
   selector: 'app-altausuarios',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AltausuariosComponent implements OnInit {
 
-  constructor() { }
+  userSignUp : FormGroup
+
+  constructor(
+    private _builder : FormBuilder,
+    private _http: HttpClient,
+  ) {
+    this.userSignUp = this._builder.group({
+      id :  ['',Validators.required],
+      nombre :  ['',Validators.required],
+      edad :  ['',Validators.required],
+      password :  ['',Validators.required]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  altaUsuario(user:Usuario){
+    console.log(user)
+    this._http.post("http://localhost:8080/usuario/save",
+    user, {responseType: "json",
+      //header},
+    })
+    .subscribe(
+      data => {
+        console.log(data)
+        alert("Alta hecha con exito")
+      },
+      error => {
+        console.log(error)
+        alert("Erorr al hace data alta")
+      }
+    )
+  }
 }
