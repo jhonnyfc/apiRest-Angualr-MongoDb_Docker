a4dc17baf5088b47c8f5aafd59f46ad221ef30fa
jhonnyfc <jhonnyfc88@gmail.com>
2021-06-03 20:51:34 +0200

    html for login and singup done

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AltausuariosComponent } from './components/altausuarios/altausuarios.component';
+import { LoginComponent } from './components/login/login.component';
+
+// ng g c components/alta --skipTests
+// ng generate service services/user
+// ng g guard guards/authv --skipTests
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo:'login'
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'altausuarios',
+    component: AltausuariosComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/components/altausuarios/altausuarios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-altausuarios',
+  templateUrl: './altausuarios.component.html',
+  styleUrls: ['./altausuarios.component.css']
+})
+export class AltausuariosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
